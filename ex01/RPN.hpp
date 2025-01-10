1a31d06dd9d4f9ccd31a4dd6a96b11819f61cc03
Emran BinJamaan <136130975+Pastifier@users.noreply.github.com>
2025-01-10 11:25:27 +0000

    Removed implementations from header file.

--- ex01/RPN.hpp
+++ ex01/RPN.hpp
@@ -6,39 +6,38 @@
 namespace RPN {
     class RPNException : public std::exception {
     public:
-        explicit RPNException(const char* message) : _msg(message) {}
-        virtual const char* what() const throw() { return _msg; }
-        virtual ~RPNException() throw() {}
+        explicit RPNException(const char* message);
+        virtual const char* what() const throw();
+        virtual ~RPNException() throw();
     private:
         const char* _msg;
     };
 
     class DivisionByZeroError : public RPNException {
     public:
-        DivisionByZeroError() : RPNException("Division by zero") {}
-        virtual ~DivisionByZeroError() throw() {}
+        DivisionByZeroError();
+        virtual ~DivisionByZeroError() throw();
     };
 
     class StackUnderflowError : public RPNException {
     public:
-        StackUnderflowError() : RPNException("Not enough operands") {}
-        virtual ~StackUnderflowError() throw() {}
+        StackUnderflowError();
+        virtual ~StackUnderflowError() throw();
     };
 
     class InvalidTokenError : public RPNException {
     public:
-        explicit InvalidTokenError(const std::string& token) 
-            : RPNException("Invalid token encountered"), _token(token) {}
-        const std::string& token() const { return _token; }
-        virtual ~InvalidTokenError() throw() {}
+        explicit InvalidTokenError(const std::string& token);
+        const std::string& token() const;
+        virtual ~InvalidTokenError() throw();
     private:
         std::string _token;
     };
 
     class ExtraOperandsError : public RPNException {
     public:
-        ExtraOperandsError() : RPNException("Extra operands left on stack") {}
-        virtual ~ExtraOperandsError() throw() {}
+        ExtraOperandsError();
+        virtual ~ExtraOperandsError() throw();
     };
 
     void processExpression(std::string const & expr);
