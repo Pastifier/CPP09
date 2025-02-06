6d264c1b1488cb87054c34924d5c2c41448c52fe
Emran BinJamaan <136130975+Pastifier@users.noreply.github.com>
2025-02-06 07:09:46 +0000

    Removed duplicate includes

--- ex00/BitcoinExchange.hpp
+++ ex00/BitcoinExchange.hpp
@@ -1,16 +1,6 @@
 #pragma once
 
-#include <map>
 #include <string>
-#include <fstream>
-#include <iostream>
-#include <cstdlib>
-
-
-#define PRINT(X) std::cout << X
-#define ERRLOG(X) std::cerr << X
-#define __FLUSH() ;std::cout << std::endl
-
 #include <iostream>
 #include <fstream>
 #include <sstream>
